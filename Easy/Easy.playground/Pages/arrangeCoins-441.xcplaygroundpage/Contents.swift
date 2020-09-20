b6e8e6b86b8f5e97f42fa987f6a9b48064aca835
inrtgdje <ttm0402@163.com>
2020-09-20 16:00:28 +0800

    Add  fizzBuzz and arrangeCoins

---
+++ Easy/Easy.playground/Pages/arrangeCoins-441.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+/*
+ 你总共有 n 枚硬币，你需要将它们摆成一个阶梯形状，第 k 行就必须正好有 k 枚硬币。
+
+ 给定一个数字 n，找出可形成完整阶梯行的总行数。
+
+ n 是一个非负整数，并且在32位有符号整型的范围内。
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/arranging-coins
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+func arrangeCoins(_ n: Int) -> Int {
+       let k = Int(sqrt(Double( 2 * n)))
+    if k * (k + 1) <= 2 * n {
+       return k
+    }else {
+        return k - 1
+    }
+}
